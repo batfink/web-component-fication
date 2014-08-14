4bc17be146fa2a2865fb752492456a38fc9c7c11
Victor Nystad <vnystad@gmail.com>
2014-08-14 12:39:11 +0200

    simplified code with Object.assign ponyfill

--- app/js/custom-elements.js
+++ app/js/custom-elements.js
@@ -1,28 +1,18 @@
 (function() {
 
-  var MegaProto = Object.create(HTMLButtonElement.prototype);
-
-  Object.defineProperty(MegaProto, 'readAttributes', {
-    value: function() {
-      this.msg = this.getAttribute('msg');
-    }
-  });
-
-  Object.defineProperty(MegaProto, 'hello', {
-    value: function() {
-      console.log(this.msg);
-    }
-  });
-
-  Object.defineProperty(MegaProto, 'createdCallback', {
-    value: function() {
-      this.readAttributes();
-      this.addEventListener('click', this.hello);
-    }
-  });
-
   document.registerElement('mega-button', {
-    prototype: MegaProto,
+    prototype: objectAssign(Object.create(HTMLButtonElement.prototype), {
+      readAttributes: function() {
+        this.msg = this.getAttribute('msg');
+      },
+      hello: function() {
+        console.log(this.msg);
+      },
+      createdCallback: function() {
+        this.readAttributes();
+        this.addEventListener('click', this.hello);
+      }
+    }),
     extends: 'button'
   })
 
