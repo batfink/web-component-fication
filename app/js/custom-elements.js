514c59fd0ad45848533d57d207abe4f9dbcdae10
Victor Nystad <vnystad@gmail.com>
2014-08-14 12:48:44 +0200

    split things up to increase readability

--- app/js/custom-elements.js
+++ app/js/custom-elements.js
@@ -1,18 +1,22 @@
 (function() {
 
+  var proto = Object.create(HTMLButtonElement.prototype);
+
+  var methods = {
+    readAttributes: function() {
+      this.msg = this.getAttribute('msg');
+    },
+    hello: function() {
+      console.log(this.msg);
+    },
+    createdCallback: function() {
+      this.readAttributes();
+      this.addEventListener('click', this.hello);
+    }
+  };
+
   document.registerElement('mega-button', {
-    prototype: objectAssign(Object.create(HTMLButtonElement.prototype), {
-      readAttributes: function() {
-        this.msg = this.getAttribute('msg');
-      },
-      hello: function() {
-        console.log(this.msg);
-      },
-      createdCallback: function() {
-        this.readAttributes();
-        this.addEventListener('click', this.hello);
-      }
-    }),
+    prototype: objectAssign(proto, methods),
     extends: 'button'
   })
 
